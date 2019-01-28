f3c881d63eaa45938548f174fe1187454849ca54
xielinhai <xielinhai@outlook.com>
2019-01-28 18:31:28 +0000

    permission

--- script/server/navigation1.sh
+++ script/server/navigation1.sh
@@ -4,4 +4,4 @@ export ROS_IP=192.168.0.118
 export ROS_MASTER_URI=http://192.168.0.118:11312
 export GAZEBO_MASTER_URI=http://192.168.0.118:11352
 
-DISPLAY=:8 vglrun -d :7.3 python script/training --model_name=rdpg
+DISPLAY=:8 vglrun -d :7.3 python script/training.py --model_name=rdpg
--- script/server/navigation2.sh
+++ script/server/navigation2.sh
@@ -4,4 +4,4 @@ export ROS_IP=192.168.0.118
 export ROS_MASTER_URI=http://192.168.0.118:11313
 export GAZEBO_MASTER_URI=http://192.168.0.118:11353
 
-DISPLAY=:8 vglrun -d :7.3 python script/training --model_name=ddpg
+DISPLAY=:8 vglrun -d :7.3 python script/training.py --model_name=ddpg
